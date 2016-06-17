32063965020dc2b2240e80aa5925f3b1edc63ac5
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-17 11:55:28 +0200

    Add helper for fetching objects for tests
    Having ID's as part of fixtures creates a problem for Postgres, so we can't have it there.
    Get's IDs through a helper instead.
    
    See DIH-89

--- test/helpers.js
+++ test/helpers.js
@@ -5,9 +5,19 @@ import path from 'path';
 
 export function loadFixtures(fixtures) {
     const f = fixtures || [
-        'users'
+        'users',
+        'destinations'
     ];
     const fixturePaths = f.map(file => `${path.resolve(__dirname)}/fixtures/${file}.json`);
     return syncDB({ force: true })
         .then(() => sequelizeFixtures.loadFiles(fixturePaths, db));
+}
+
+/**
+ * getAllDestinationElements - Gives you one of the fixture elements of a given type
+ *
+ * @return {Array}  - All fixture elements form db
+ */
+export function getAllDestinationElements() {
+    return db.Destination.findAll();
 }
