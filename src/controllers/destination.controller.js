49fa36495e99d68070001e717773345abada66be
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-17 12:01:31 +0200

    Add controller methods for destinations API
    See DIH-89

--- src/controllers/destination.controller.js
+++ src/controllers/destination.controller.js
@@ -1,7 +1,44 @@
+import Sequelize from 'sequelize';
 import db from '../models';
+import { ResourceNotFoundError, ValidationError } from '../components/errors';
 
 export function list(req, res, next) {
     db.Destination.findAll()
         .then(res.json.bind(res))
+        .catch(next);
+}
+
+export function create(req, res, next) {
+    db.Destination.create(req.body)
+        .then((savedObj) => {
+            res.status(201).json(savedObj);
+        })
+        .catch(Sequelize.ValidationError, err => {
+            throw new ValidationError(err);
+        })
+        .catch(next);
+}
+
+export function remove(req, res, next) {
+    db.Destination.findOne({ id: req.params.id })
+        .then(item => {
+            if (!item) {
+                throw new ResourceNotFoundError();
+            }
+            item.destroy();
+        })
+        .then(res.sendStatus(200))
+        .catch(next);
+}
+
+export function update(req, res, next) {
+    db.Destination.findOne({ id: req.params.id })
+        .then(item => {
+            item.update(req.body);
+        })
+        .then(res.sendStatus(204))
+        .catch(Sequelize.ValidationError, err => {
+            throw new ValidationError(err);
+        })
         .catch(next);
 }
