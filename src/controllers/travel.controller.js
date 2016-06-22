6ed1b6923d2205b8ee680a31046e4f591a460c02
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-22 14:40:49 +0200

    Set up controller and routes for Travels
    See DIH-104

---
+++ src/controllers/travel.controller.js
@@ -0,0 +1,55 @@
+import Sequelize from 'sequelize';
+import db from '../models';
+import _ from 'lodash';
+import { ResourceNotFoundError, ValidationError } from '../components/errors';
+
+export function list(req, res, next) {
+    db.Travel.findAll()
+        .then(res.json.bind(res))
+        .catch(next);
+}
+
+export function create(req, res, next) {
+    db.Travel.create(req.body)
+        .then(savedObj => res.status(201).json(savedObj))
+        .catch(Sequelize.ValidationError, err => {
+            throw new ValidationError(err);
+        })
+        .catch(next);
+}
+
+export function destroy(req, res, next) {
+    db.Travel.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(count => {
+        if (count === 0) {
+            throw new ResourceNotFoundError('travel');
+        }
+    })
+    .then(() => res.sendStatus(200))
+    .catch(next);
+}
+
+export function update(req, res, next) {
+    db.Travel.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+        fields: _.without(Object.keys(req.body), 'id')
+    })
+    .then(count => {
+        console.log(count);
+        if (!count) throw new ResourceNotFoundError('travel');
+        res.sendStatus(204);
+    })
+    .catch(Sequelize.ValidationError, err => {
+        throw new ValidationError(err);
+    })
+    .catch(Sequelize.DatabaseError, err => {
+        throw new ValidationError(err);
+    })
+    .catch(next);
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import user from './user.routes';
 import destination from './destination.routes';
+import travel from './travel.routes';
 import { pageNotFoundMiddleware, errorMiddleware } from '../components/errors';
 
 const router = express.Router();
 
 router.use('/users', user);
 router.use('/destinations', destination);
+router.use('/travels', travel);
 
 router.use(pageNotFoundMiddleware);
 router.use(errorMiddleware);
