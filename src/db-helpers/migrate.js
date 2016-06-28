3739ee52c2ee0f00756251c9904e29e3c658053e
Kristoffer Larsen <kristoffer@larsen.so>
2016-06-28 15:15:30 +0200

    Add missing documentation
    See DIH-130

--- src/db-helpers/migrate.js
+++ src/db-helpers/migrate.js
@@ -1,5 +1,17 @@
+/**
+ * Creates and exports and umzug class to be used to migrate the database
+ * @module db-helpers/migrate
+ */
 import Umzug from 'umzug';
 
+/**
+ * default - exports default function
+ *
+ * @function default
+ * @memberof  module:db-helpers/migrate
+ * @param  {SequlizeDBInstance} sequlizeInstance - The database to be migrated, create from models
+ * @return {Umzug} - Used to migrate database
+ */
 export default function (sequlizeInstance) {
     return new Umzug({
 
