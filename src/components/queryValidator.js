6904746652f7297cf7dce55c9e797ee5a3742765
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-06-29 13:37:43 +0200

    Add docs
    See DIH-156

--- src/components/queryValidator.js
+++ src/components/queryValidator.js
@@ -1,4 +1,16 @@
+/**
+ * Generic validation of query objects
+ * @module components/queryValidator
+ */
 
+/**
+ * validateQuery - Catches all upstream errors and returns them to the requester.
+ *
+ * @param  {Object} query - Express req.query object
+ * @param  {Array} allowedQueryParams - List of names of model properties that
+ * are allowed to be queried on
+ * @return {Boolean}  - If the query is valid or not
+ */
 export function validateQuery(query, allowedQueryParams) {
     const keys = Object.keys(query);
     let isValid = true;
